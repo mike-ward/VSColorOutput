2d5d2904b4d27e16138202e2fb8cafffe1e5b98b
blueonion <blueonion@localhost>
2011-12-05 18:24:37 -0500

    Unit tests for FontAndColorStorage and TextManagerEvents

--- Tests/FontAndColorStorageTests.cs
+++ Tests/FontAndColorStorageTests.cs
@@ -19,12 +19,29 @@ namespace Tests
                 FontAndColorStorage.Override = mockStore.Object;
                 FontAndColorStorage.UpdateColors();
 
-                //const uint flags = (uint)(
-                //    __FCSTORAGEFLAGS.FCSF_LOADDEFAULTS |
-                //        __FCSTORAGEFLAGS.FCSF_NOAUTOCOLORS |
-                //            __FCSTORAGEFLAGS.FCSF_PROPAGATECHANGES);
+                const uint flags = (uint)(
+                    __FCSTORAGEFLAGS.FCSF_LOADDEFAULTS |
+                        __FCSTORAGEFLAGS.FCSF_NOAUTOCOLORS |
+                            __FCSTORAGEFLAGS.FCSF_PROPAGATECHANGES);
 
-                //mockStore.Verify(s => s.OpenCategory(DefGuidList.guidTextEditorFontCategory, flags));
+                var textEditorGuid = DefGuidList.guidTextEditorFontCategory;
+                mockStore.Verify(s => s.OpenCategory(ref textEditorGuid, flags));
+                mockStore.Verify(s => s.GetItem(OutputClassificationDefinitions.BuildHead, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.GetItem(OutputClassificationDefinitions.BuildText, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.GetItem(OutputClassificationDefinitions.LogInfo, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.GetItem(OutputClassificationDefinitions.LogWarn, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.GetItem(OutputClassificationDefinitions.LogError, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.GetItem(OutputClassificationDefinitions.LogSpecial, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.CloseCategory(), Times.Exactly(2));
+
+                var ouputWindowGuid = DefGuidList.guidOutputWindowFontCategory;
+                mockStore.Verify(s => s.OpenCategory(ref ouputWindowGuid, flags));
+                mockStore.Verify(s => s.SetItem(OutputClassificationDefinitions.BuildHead, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.SetItem(OutputClassificationDefinitions.BuildText, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.SetItem(OutputClassificationDefinitions.LogInfo, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.SetItem(OutputClassificationDefinitions.LogWarn, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.SetItem(OutputClassificationDefinitions.LogError, It.IsAny<ColorableItemInfo[]>()));
+                mockStore.Verify(s => s.SetItem(OutputClassificationDefinitions.LogSpecial, It.IsAny<ColorableItemInfo[]>()));
             }
             finally
             {
--- Tests/TextManagerEventsTests.cs
+++ Tests/TextManagerEventsTests.cs
@@ -1,7 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿// Copyright (c) 2011 Blue Onion Software, All rights reserved
+using System;
+using System.Runtime.InteropServices;
+using BlueOnionSoftware;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Moq;
 using NUnit.Framework;
 
 namespace Tests
@@ -9,9 +13,70 @@ namespace Tests
     [TestFixture]
     public class TextManagerEventsTests
     {
-        public void UserPrefrencesChangedUpdateColors()
+        [Test]
+        public void UserPrefrencesChangedUpdateColorsForTextEditorCategory()
         {
-            
+            var textManagerEvents = new TextManagerEvents();
+            var fontResource = new FONTCOLORPREFERENCES
+            {
+                pguidFontCategory = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Guid))),
+                pguidColorService = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Guid))),
+                pColorTable = Marshal.AllocHGlobal(1)
+            };
+            Marshal.StructureToPtr(DefGuidList.guidTextEditorFontCategory, fontResource.pguidFontCategory, true);
+            Marshal.StructureToPtr(Guid.NewGuid(), fontResource.pguidColorService, true);
+            var fontResources = new[] { fontResource };
+            try
+            {
+                var mockStore = new Mock<IVsFontAndColorStorage>();
+                FontAndColorStorage.Override = mockStore.Object;
+                textManagerEvents.OnUserPreferencesChanged(null, null, null, fontResources);
+
+                const uint flags = (uint)(
+                    __FCSTORAGEFLAGS.FCSF_LOADDEFAULTS |
+                        __FCSTORAGEFLAGS.FCSF_NOAUTOCOLORS |
+                            __FCSTORAGEFLAGS.FCSF_PROPAGATECHANGES);
+
+                var textEditorGuid = DefGuidList.guidTextEditorFontCategory;
+                mockStore.Verify(s => s.OpenCategory(ref textEditorGuid, flags));
+            }
+            finally
+            {
+                FontAndColorStorage.Override = null;
+            }
+        }
+
+        [Test]
+        public void UserPrefrencesChangedNotUpdateColorsForNotTextEditorCategory()
+        {
+            try
+            {
+                var textManagerEvents = new TextManagerEvents();
+                var fontResource = new FONTCOLORPREFERENCES
+                {
+                    pguidFontCategory = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Guid))),
+                    pguidColorService = Marshal.AllocHGlobal(Marshal.SizeOf(typeof(Guid))),
+                    pColorTable = Marshal.AllocHGlobal(1)
+                };
+                Marshal.StructureToPtr(DefGuidList.guidPrinterFontCategory, fontResource.pguidFontCategory, true);
+                Marshal.StructureToPtr(Guid.NewGuid(), fontResource.pguidColorService, true);
+                var fontResources = new[] { fontResource };
+                var mockStore = new Mock<IVsFontAndColorStorage>();
+                FontAndColorStorage.Override = mockStore.Object;
+                textManagerEvents.OnUserPreferencesChanged(null, null, null, fontResources);
+
+                const uint flags = (uint)(
+                    __FCSTORAGEFLAGS.FCSF_LOADDEFAULTS |
+                        __FCSTORAGEFLAGS.FCSF_NOAUTOCOLORS |
+                            __FCSTORAGEFLAGS.FCSF_PROPAGATECHANGES);
+
+                var textEditorGuid = DefGuidList.guidTextEditorFontCategory;
+                mockStore.Verify(s => s.OpenCategory(ref textEditorGuid, flags), Times.Never());
+            }
+            finally
+            {
+                FontAndColorStorage.Override = null;
+            }
         }
     }
 }
