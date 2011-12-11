667565922f2f476713c75e9f9c23c607030077ce
blueonion <blueonion@localhost>
2011-12-11 09:32:22 -0500

    Opions loading and saving. Does not affect classifier (yet)

--- VSColorOutput/VSColorOutputOptions.cs
+++ VSColorOutput/VSColorOutputOptions.cs
@@ -1,16 +1,90 @@
 ﻿// Copyright (c) 2011 Blue Onion Software, All rights reserved
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Json;
+using System.Text;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
 
 namespace BlueOnionSoftware
 {
+    [Guid("BE905985-26BB-492B-9453-743E26F4E8BB")]
     public class VsColorOutputOptions : DialogPage
     {
-        public PatternClassificationType[] Patterns { get; set; }
-    }
+        public const string Category = "VSColorOutput";
+        public const string SubCategory = "General";
+        private const string RegExPatternsKey = "RegExPatterns";
 
-    public class PatternClassificationType
-    {
-        public string Pattern { get; set; }
-        public string ClassificationType { get; set; }
+        [DisplayName("RegEx Patterns")]
+        public RegExClassification[] RegExPatterns { get; set; }
+
+        public override void LoadSettingsFromStorage()
+        {
+            using (var root = VSRegistry.RegistryRoot(__VsLocalRegistryType.RegType_UserSettings, false))
+            using (var settings = root.OpenSubKey(SettingsRegistryPath))
+            {
+                var json = settings.GetValue(RegExPatternsKey) as string;
+                RegExPatterns = string.IsNullOrEmpty(json) ? DefaultPatterns() : LoadPatternsFromRegistry(json);
+            }
+        }
+
+        public override void SaveSettingsToStorage()
+        {
+            using (var ms = new MemoryStream())
+            {
+                var serializer = new DataContractJsonSerializer(typeof(RegExClassification[]));
+                serializer.WriteObject(ms, RegExPatterns);
+                var json = Encoding.Default.GetString(ms.ToArray());
+                using (var root = VSRegistry.RegistryRoot(__VsLocalRegistryType.RegType_UserSettings, false))
+                using (var settings = root.OpenSubKey(SettingsRegistryPath, true))
+                {
+                    settings.SetValue(RegExPatternsKey, json, RegistryValueKind.String);
+                    settings.Close();
+                }
+            }
+        }
+
+        private static RegExClassification[] DefaultPatterns()
+        {
+            return new[]
+            {
+                new RegExClassification {RegExPattern = "=====", ClassificationType = ClassificationTypes.BuildHead, IgnoreCase = true}
+            };
+        }
+
+        private static RegExClassification[] LoadPatternsFromRegistry(string json)
+        {
+            using (var ms = new MemoryStream(Encoding.Unicode.GetBytes(json)))
+            {
+                var serializer = new DataContractJsonSerializer(typeof(RegExClassification[]));
+                return (RegExClassification[])serializer.ReadObject(ms);
+            }
+        }
+
+        [Serializable]
+        public enum ClassificationTypes
+        {
+            BuildText,
+            BuildHead,
+            LogError,
+            LogWarning,
+            LogInformation,
+            LogSpecial
+        }
+
+        public class RegExClassification
+        {
+            public string RegExPattern { get; set; }
+            public ClassificationTypes ClassificationType { get; set; }
+            public bool IgnoreCase { get; set; }
+
+            public override string ToString()
+            {
+                return string.Format("\"{0}\",{1},{2}", RegExPattern, ClassificationType, IgnoreCase);
+            }
+        }
     }
 }
--- VSColorOutput/VsColorOutputPackage.cs
+++ VSColorOutput/VsColorOutputPackage.cs
@@ -1,26 +1,17 @@
 ﻿// Copyright (c) 2011 Blue Onion Software, All rights reserved
-using System.Diagnostics;
-using System.Globalization;
 using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.Shell;
+using Microsoft.Win32;
 
 namespace BlueOnionSoftware
 {
     [Guid("CD56B219-38CB-482A-9B2D-7582DF4AAF1E")]
+    [DefaultRegistryRoot("Software\\Microsoft\\VisualStudio\\10.0")]
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [ProvideOptionPage(typeof(VsColorOutputOptions), "VSColorOutput", "Options", 1000, 1001, false)]
+    [ProvideOptionPage(typeof(VsColorOutputOptions), VsColorOutputOptions.Category, VsColorOutputOptions.SubCategory, 1000, 1001, true)]
+    [ProvideProfile(typeof(VsColorOutputOptions), VsColorOutputOptions.Category, VsColorOutputOptions.SubCategory, 1000, 1001, true)]
     [InstalledProductRegistration("VSColorOutput", "Color output for build and debug windows", "VSColorOutput - Blue Onion Software")]
     public class VsColorOutputPackage : Package
     {
-        public VsColorOutputPackage()
-        {
-            Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering constructor for: {0}", this));
-        }
-
-        protected override void Initialize()
-        {
-            Trace.WriteLine(string.Format(CultureInfo.CurrentCulture, "Entering Initialize() of: {0}", this));
-            base.Initialize();
-        }
     }
 }
