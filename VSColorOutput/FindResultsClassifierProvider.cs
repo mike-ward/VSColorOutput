4d42f2132beb8662f6198767aac86e01d34b2d02
mattellis <mattellis@localhost>
2012-02-08 08:07:36 +0000

    Work in progress find results classifier. Highlights multiple hits of the search term per line. Respects case sensitive flag

--- VSColorOutput/FindResultsClassifierProvider.cs
+++ VSColorOutput/FindResultsClassifierProvider.cs
@@ -3,17 +3,23 @@ using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
 using Microsoft.VisualStudio.Utilities;
 
+// Unassigned variables
+#pragma warning disable 649
+
 namespace BlueOnionSoftware
 {
-    [ContentType("code")]
+    [ContentType("FindResults")]
     [Export(typeof(IClassifierProvider))]
     public class FindResultsClassifierProvider : IClassifierProvider
     {
+        [Import]
+        internal IClassificationTypeRegistryService ClassificationRegistry;
+
         public static IClassifier FindResultsClassifier { get; private set; }
 
         public IClassifier GetClassifier(ITextBuffer textBuffer)
         {
-            return FindResultsClassifier ?? (FindResultsClassifier = new FindResultsClassifier());
+            return FindResultsClassifier ?? (FindResultsClassifier = new FindResultsClassifier(ClassificationRegistry));
         }
     }
 }
