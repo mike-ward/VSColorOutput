c77609f41a7a9841154cea372d0c541ffd467edf
mattellis <mattellis@localhost>
2012-02-08 08:07:36 +0000

    Work in progress find results classifier. Highlights multiple hits of the search term per line. Respects case sensitive flag

---
+++ Tests/FakeTextSnapshot.cs
@@ -0,0 +1,328 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+
+namespace Tests
+{
+    public class FakeTextSnapshot : ITextSnapshot
+    {
+        private readonly string text;
+        private readonly string[] lines;
+
+        public FakeTextSnapshot(string text)
+        {
+            this.text = text;
+            lines = text.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+
+            TextBuffer = new FakeTextBuffer();
+            Version = new FakeVersion(1);
+        }
+
+        public string GetText(Span span)
+        {
+            return text.Substring(span.Start, span.Length);
+        }
+
+        public string GetText(int startIndex, int length)
+        {
+            return text.Substring(startIndex, text.Length);
+        }
+
+        public string GetText()
+        {
+            return text;
+        }
+
+        public char[] ToCharArray(int startIndex, int length)
+        {
+            return GetText(startIndex, length).ToCharArray();
+        }
+
+        public ITextSnapshotLine GetLineFromLineNumber(int lineNumber)
+        {
+            var position = GetPositionFromLineNumber(lineNumber);
+            return new FakeTextSnapshotLine(this, lines[lineNumber], position, lineNumber);
+        }
+
+        public ITextSnapshotLine GetLineFromPosition(int position)
+        {
+            var lineNumber = GetLineNumberFromPosition(position);
+            return new FakeTextSnapshotLine(this, lines[lineNumber], position, lineNumber);
+        }
+
+        public int GetLineNumberFromPosition(int position)
+        {
+            var count = 0;
+            for (var i = 0; i < LineCount; i++)
+            {
+                count += lines[i].Length + Environment.NewLine.Length;
+                if (count > position)
+                    return i;
+            }
+
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private int GetPositionFromLineNumber(int lineNumber)
+        {
+            var position = 0;
+            for (var i = 0; i < lineNumber; i++)
+                position += lines[i].Length + Environment.NewLine.Length;
+            return position;
+        }
+
+        public int Length
+        {
+            get { return text.Length; }
+        }
+
+        public int LineCount
+        {
+            get { return lines.Length; }
+        }
+
+        public char this[int position]
+        {
+            get { return text[position]; }
+        }
+
+        public ITextBuffer TextBuffer { get; private set; }
+        public ITextVersion Version { get; private set; }
+
+        #region Not required or implemented
+
+        public void CopyTo(int sourceIndex, char[] destination, int destinationIndex, int count)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackingPoint CreateTrackingPoint(int position, PointTrackingMode trackingMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackingPoint CreateTrackingPoint(int position, PointTrackingMode trackingMode, TrackingFidelityMode trackingFidelity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackingSpan CreateTrackingSpan(Span span, SpanTrackingMode trackingMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackingSpan CreateTrackingSpan(Span span, SpanTrackingMode trackingMode, TrackingFidelityMode trackingFidelity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackingSpan CreateTrackingSpan(int start, int length, SpanTrackingMode trackingMode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ITrackingSpan CreateTrackingSpan(int start, int length, SpanTrackingMode trackingMode, TrackingFidelityMode trackingFidelity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(TextWriter writer, Span span)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Write(TextWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IContentType ContentType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IEnumerable<ITextSnapshotLine> Lines
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        // Only here so that ToString will work in SnapshotSpan for debugging
+        private class FakeTextBuffer : ITextBuffer
+        {
+            public FakeTextBuffer()
+            {
+                Properties = new PropertyCollection();
+            }
+
+            public PropertyCollection Properties { get; private set; }
+
+            public ITextEdit CreateEdit(EditOptions options, int? reiteratedVersionNumber, object editTag)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITextEdit CreateEdit()
+            {
+                throw new NotImplementedException();
+            }
+
+            public IReadOnlyRegionEdit CreateReadOnlyRegionEdit()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void TakeThreadOwnership()
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool CheckEditAccess()
+            {
+                throw new NotImplementedException();
+            }
+
+            public void ChangeContentType(IContentType newContentType, object editTag)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITextSnapshot Insert(int position, string text)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITextSnapshot Delete(Span deleteSpan)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITextSnapshot Replace(Span replaceSpan, string replaceWith)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsReadOnly(int position)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsReadOnly(int position, bool isEdit)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsReadOnly(Span span)
+            {
+                throw new NotImplementedException();
+            }
+
+            public bool IsReadOnly(Span span, bool isEdit)
+            {
+                throw new NotImplementedException();
+            }
+
+            public NormalizedSpanCollection GetReadOnlyExtents(Span span)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IContentType ContentType
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public ITextSnapshot CurrentSnapshot
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public bool EditInProgress
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public event EventHandler<SnapshotSpanEventArgs> ReadOnlyRegionsChanged;
+            public event EventHandler<TextContentChangedEventArgs> Changed;
+            public event EventHandler<TextContentChangedEventArgs> ChangedLowPriority;
+            public event EventHandler<TextContentChangedEventArgs> ChangedHighPriority;
+            public event EventHandler<TextContentChangingEventArgs> Changing;
+            public event EventHandler PostChanged;
+            public event EventHandler<ContentTypeChangedEventArgs> ContentTypeChanged;
+        }
+
+        private class FakeVersion : ITextVersion
+        {
+            public FakeVersion(int versionNumber)
+            {
+                VersionNumber = versionNumber;
+            }
+
+            public ITrackingPoint CreateTrackingPoint(int position, PointTrackingMode trackingMode)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITrackingPoint CreateTrackingPoint(int position, PointTrackingMode trackingMode, TrackingFidelityMode trackingFidelity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITrackingSpan CreateTrackingSpan(Span span, SpanTrackingMode trackingMode)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITrackingSpan CreateTrackingSpan(Span span, SpanTrackingMode trackingMode, TrackingFidelityMode trackingFidelity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITrackingSpan CreateTrackingSpan(int start, int length, SpanTrackingMode trackingMode)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITrackingSpan CreateTrackingSpan(int start, int length, SpanTrackingMode trackingMode, TrackingFidelityMode trackingFidelity)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITrackingSpan CreateCustomTrackingSpan(Span span, TrackingFidelityMode trackingFidelity, object customState, CustomTrackToVersion behavior)
+            {
+                throw new NotImplementedException();
+            }
+
+            public ITextVersion Next
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int Length
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public INormalizedTextChangeCollection Changes
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public ITextBuffer TextBuffer
+            {
+                get { throw new NotImplementedException(); }
+            }
+
+            public int VersionNumber { get; private set; }
+
+            public int ReiteratedVersionNumber
+            {
+                get { throw new NotImplementedException(); }
+            }
+        }
+
+        #endregion
+    }
+
+}
--- VSColorOutput/FindResultsClassifier.cs
+++ VSColorOutput/FindResultsClassifier.cs
@@ -1,22 +1,99 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.Text;
 using Microsoft.VisualStudio.Text.Classification;
+
+// Disable warning for unused ClassificationChanged event
+#pragma warning disable 67
 
 namespace BlueOnionSoftware
 {
     public class FindResultsClassifier : IClassifier
     {
+        private readonly IClassificationTypeRegistryService classificationRegistry;
+        private static readonly Regex SearchOptionsRegex;
+        private static Regex FilenameRegex;
+
+        private SearchOptions searchOptions;
+        private Regex searchTextRegex;
+
+        static FindResultsClassifier()
+        {
+            // TODO: What about localised instances of VS?
+            SearchOptionsRegex = new Regex("Find all \"(?<searchTerm>[^\"]+)\", (?<casing>Match case)?", RegexOptions.Compiled);
+            FilenameRegex = new Regex(@"^(.*:.*\(\d+\):)", RegexOptions.Compiled);
+        }
+
+        public FindResultsClassifier(IClassificationTypeRegistryService classificationRegistry)
+        {
+            this.classificationRegistry = classificationRegistry;
+        }
+
         public IList<ClassificationSpan> GetClassificationSpans(SnapshotSpan span)
         {
-            // Quick fix to stop unused instance compiler error. Don't worry, it won't last
-            if (ClassificationChanged == null)
+            var classifications = new List<ClassificationSpan>();
+
+            var snapshot = span.Snapshot;
+            if (snapshot == null || snapshot.Length == 0)
             {
+                return classifications;
             }
 
-            return new List<ClassificationSpan>();
+            ResetSearchOptions(span);
+            if (searchOptions != null)
+            {
+                var text = span.GetText();
+
+                classifications.AddRange(GetMatches(text, searchTextRegex, span.Start, SearchTermClassificationType));
+                //classifications.AddRange(GetMatches(text, FilenameRegex, span.Start, filenameClassificationType));
+            }   
+         
+            return classifications;
+        }
+
+        private void ResetSearchOptions(SnapshotSpan span)
+        {
+            if (span.Start.Position == 0)
+            {
+                searchOptions = null;
+
+                var firstLine = span.Snapshot.GetLineFromLineNumber(0).GetText();
+                var match = SearchOptionsRegex.Match(firstLine);
+                if (match.Success)
+                {
+                    searchOptions = new SearchOptions
+                                        {
+                                            SearchTerm = match.Groups["searchTerm"].Value,
+                                            MatchCase = match.Groups["casing"].Success
+                                        };
+                    var casing = searchOptions.MatchCase ? RegexOptions.None : RegexOptions.IgnoreCase;
+                    searchTextRegex = new Regex(searchOptions.SearchTerm, RegexOptions.None | casing);
+                }
+            }
+        }
+
+        private IEnumerable<ClassificationSpan> GetMatches(string text, Regex regex, SnapshotPoint snapStart, IClassificationType classificationType)
+        {
+            return from match in regex.Matches(text).Cast<Match>()
+                   select new ClassificationSpan(new SnapshotSpan(snapStart + match.Index, match.Length), classificationType);
+        }
+
+        private IClassificationType SearchTermClassificationType
+        {
+            get { return classificationRegistry.GetClassificationType(OutputClassificationDefinitions.FindResultsSearchTerm); }
         }
 
         public event EventHandler<ClassificationChangedEventArgs> ClassificationChanged;
+
+        private class SearchOptions
+        {
+            public string SearchTerm;
+            public bool MatchCase;
+            //public bool MatchWholeWord;
+            //public bool UsingRegex;
+            //public bool UsingWildcards;     // What the heck is this?
+        }
     }
 }
