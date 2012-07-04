bf881a5aac216b6f7da67016f9759da0968d957e
axxie <axxie@localhost>
2012-07-04 16:27:53 +0300

    Added unit test for projects build report feature. Existing unit test for time elapsed report is updated to check the value of passed string parameter. Parameter must match the string used in main code.

--- Tests/BuildEventsTests.cs
+++ Tests/BuildEventsTests.cs
@@ -90,8 +90,8 @@ namespace Tests
             mockToolWindows.SetupGet(t => t.OutputWindow).Returns(mockOutputWindow.Object);
             mockOutputWindow.SetupGet(o => o.OutputWindowPanes).Returns(mockOutputWindowPanes.Object);
             mockOutputWindowPane.SetupGet(op => op.Guid).Returns(VSConstants.OutputWindowPaneGuid.BuildOutputPane_string);
-            mockOutputWindowPane.Setup(op => op.OutputString(It.IsAny<string>()));
-            var panes = new [] {mockOutputWindowPane.Object};
+            mockOutputWindowPane.Setup(op => op.OutputString(It.IsRegex("Time Elapsed .*")));
+            var panes = new[] { mockOutputWindowPane.Object };
             mockOutputWindowPanes.Setup(op => op.GetEnumerator()).Returns(panes.GetEnumerator());
             mockEvents.SetupGet(e => e.DTEEvents).Returns(mockDteEvents.Object);
             mockEvents.SetupGet(e => e.BuildEvents).Returns(() => mockBuildEvents.Object);
@@ -112,6 +112,49 @@ namespace Tests
             mockOutputWindowPanes.VerifyAll();
         }
 
+        [Test]
+        public void OnBuildDoneShowsProjectsBuildReportWhenEnabled()
+        {
+            var mockDte2 = new Mock<DTE2>();
+            var mockEvents = new Mock<Events>();
+            var mockDteEvents = new Mock<DTEEvents>();
+            var mockBuildEvents = new Mock<BuildEvents>();
+            var mockServiceProvider = new Mock<IServiceProvider>();
+            var mockToolWindows = new Mock<ToolWindows>();
+            var mockOutputWindow = new Mock<OutputWindow>();
+            var mockOutputWindowPanes = new Mock<OutputWindowPanes>();
+            var mockOutputWindowPane = new Mock<OutputWindowPane>();
+            mockServiceProvider.Setup(sp => sp.GetService(typeof(DTE))).Returns(mockDte2.Object);
+            mockDte2.SetupGet(d => d.Events).Returns(mockEvents.Object);
+            mockDte2.SetupGet(d => d.ToolWindows).Returns(mockToolWindows.Object);
+            mockToolWindows.SetupGet(t => t.OutputWindow).Returns(mockOutputWindow.Object);
+            mockOutputWindow.SetupGet(o => o.OutputWindowPanes).Returns(mockOutputWindowPanes.Object);
+            mockOutputWindowPane.SetupGet(op => op.Guid).Returns(VSConstants.OutputWindowPaneGuid.BuildOutputPane_string);
+            mockOutputWindowPane.Setup(op => op.OutputString(It.Is<string>(s => s =="\r\nProjects build report:\r\n")));
+            mockOutputWindowPane.Setup(op => op.OutputString(It.Is<string>(s => s == "  Succeeded | test.proj [Debug|Any CPU]\r\n")));
+            mockOutputWindowPane.Setup(op => op.OutputString(It.Is<string>(s => s == "  Failed    | test2.proj [Release|Any CPU]\r\n")));
+            var panes = new[] { mockOutputWindowPane.Object };
+            mockOutputWindowPanes.Setup(op => op.GetEnumerator()).Returns(panes.GetEnumerator());
+            mockEvents.SetupGet(e => e.DTEEvents).Returns(mockDteEvents.Object);
+            mockEvents.SetupGet(e => e.BuildEvents).Returns(() => mockBuildEvents.Object);
+
+            var buildEvents = new BlueOnionSoftware.BuildEvents(mockServiceProvider.Object);
+            buildEvents.ShowBuildReport = true;
+            mockBuildEvents.Raise(be => be.OnBuildBegin += null, vsBuildScope.vsBuildScopeSolution, vsBuildAction.vsBuildActionBuild);
+            mockBuildEvents.Raise(be => be.OnBuildProjConfigDone += null, "test.proj", "Debug", "Any CPU", "", true);
+            mockBuildEvents.Raise(be => be.OnBuildProjConfigDone += null, "test2.proj", "Release", "Any CPU", "", false);
+            mockBuildEvents.Raise(be => be.OnBuildDone += null, vsBuildScope.vsBuildScopeSolution, vsBuildAction.vsBuildActionBuild);
+
+            mockDte2.VerifyAll();
+            mockEvents.VerifyAll();
+            mockDteEvents.VerifyAll();
+            mockBuildEvents.VerifyAll();
+            mockServiceProvider.VerifyAll();
+            mockToolWindows.VerifyAll();
+            mockOutputWindow.VerifyAll();
+            mockOutputWindowPane.VerifyAll();
+            mockOutputWindowPanes.VerifyAll();
+        }
         [Test]
         public void DteEventsModeChangeActivatesDebugOutputWindowWhenEnabled()
         {
