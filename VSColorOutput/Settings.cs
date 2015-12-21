e914cfa07e2289584b05521d1886af9543486f79
mike-ward <mike@mike-ward.net>
2015-12-20 22:49:47 -0500

    start adding surrogate members to serialize colors as string

--- VSColorOutput/Settings.cs
+++ VSColorOutput/Settings.cs
@@ -1,36 +1,75 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.IO;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Json;
 using System.Windows.Media;
+using static System.Environment;
 
 namespace BlueOnionSoftware
 {
+    [DataContract]
     public class Settings
     {
         public const string RegistryPath = @"DialogPage\BlueOnionSoftware.VsColorOutputOptions";
 
+        private static Color FromString(string value, Color defaultValue) => (Color?)ColorConverter.ConvertFromString(value) ?? defaultValue;
+
+        [DataMember(Order = 0)]
         public bool EnableStopOnBuildError { get; set; }
+
+        [DataMember(Order = 1)]
         public bool ShowElapsedBuildTime { get; set; }
+
+        [DataMember(Order = 2)]
         public bool ShowBuildReport { get; set; }
+
+        [DataMember(Order = 3)]
         public bool ShowDebugWindowOnDebug { get; set; }
+
+        [DataMember(Order = 4)]
         public bool HighlightFindResults { get; set; }
 
+        [DataMember(Order = 5)]
         public RegExClassification[] Patterns { get; set; } = DefaultPatterns();
 
+        [IgnoreDataMember]
         public Color BuildMessageColor { get; set; } = Colors.Green;
+
+        [DataMember(Name = "BuildMessageColor", Order = 6)]
+        public string BuildMessageColorAsString
+        {
+            get { return BuildMessageColor.ToString(); }
+            set { BuildMessageColor = FromString(value, BuildMessageColor); }
+        }
+
+        [DataMember(Order = 7)]
         public Color BuildTextColor { get; set; } = Colors.Gray;
 
+        [DataMember(Order = 8)]
         public Color ErrorTextColor { get; set; } = Colors.Red;
+
+        [DataMember(Order = 9)]
         public Color WarningTextColor { get; set; } = Colors.Olive;
+
+        [DataMember(Order = 10)]
         public Color InformationTextColor { get; set; } = Colors.DarkBlue;
 
+        [DataMember(Order = 11)]
         public Color CustomTextColor1 { get; set; } = Colors.Purple;
+
+        [DataMember(Order = 12)]
         public Color CustomTextColor2 { get; set; } = Colors.DarkSalmon;
+
+        [DataMember(Order = 13)]
         public Color CustomTextColor3 { get; set; } = Colors.DarkOrange;
+
+        [DataMember(Order = 14)]
         public Color CustomTextColor4 { get; set; } = Colors.Brown;
 
+        [DataMember(Order = 15)]
         public Color FindSearchTermColor { get; set; } = Colors.Green;
+
+        [DataMember(Order = 16)]
         public Color FindFileNameColor { get; set; } = Colors.Gray;
 
         private static readonly string ProgramDataFolder;
@@ -42,7 +81,7 @@ namespace BlueOnionSoftware
 
         static Settings()
         {
-            ProgramDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "VSColorOutput");
+            ProgramDataFolder = Path.Combine(GetFolderPath(SpecialFolder.ApplicationData), "VSColorOutput");
             SettingsFile = Path.Combine(ProgramDataFolder, "settings.json");
         }
 
