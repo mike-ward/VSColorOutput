fd7dd1f6707a7eb692163099ae602c5c5b80f682
mike-ward <mike@mike-ward.net>
2015-12-22 18:12:13 -0500

    v2.0.3-beta4

--- VSColorOutput/Properties/AssemblyInfo.cs
+++ VSColorOutput/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("VSColorOutput is a trademark of Mike Ward")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.0.2")]
-[assembly: AssemblyFileVersion("2.0.2")]
+[assembly: AssemblyVersion("2.0.3")]
+[assembly: AssemblyFileVersion("2.0.3")]
 [assembly: Guid("9D5A1B6F-D173-424E-B126-7A459F13DB7A")]
 
