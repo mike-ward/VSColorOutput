65769bc79a2dd4d6698a5d74191f0d3d5f5a94fc
mike-ward <mike@mike-ward.net>
2015-12-29 09:24:08 -0500

    use .NET 4.5.2 (#12)

--- VSColorOutput/VsColorOutputPackage.cs
+++ VSColorOutput/VsColorOutputPackage.cs
@@ -8,7 +8,7 @@ namespace BlueOnionSoftware
     [PackageRegistration(UseManagedResourcesOnly = true)]
     [ProvideOptionPage(typeof(VsColorOutputOptionsDialog), VsColorOutputOptionsDialog.Category, VsColorOutputOptionsDialog.SubCategory, 1000, 1001, true)]
     [ProvideProfile(typeof(VsColorOutputOptionsDialog), VsColorOutputOptionsDialog.Category, VsColorOutputOptionsDialog.SubCategory, 1000, 1001, true)]
-    [InstalledProductRegistration("VSColorOutput", "Color output for build and debug windows - http://mike-ward.net/vscoloroutput", "2.1.0")]
+    [InstalledProductRegistration("VSColorOutput", "Color output for build and debug windows - http://mike-ward.net/vscoloroutput", "2.1.1")]
     public class VsColorOutputPackage : Package
     {
     }
