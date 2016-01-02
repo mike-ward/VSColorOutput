3f0cc30137d496f88ecb538c9d9ba3c4780154fd
mike-ward <mike@mike-ward.net>
2016-01-02 12:57:55 -0500

    stub in time stamp margin

---
+++ VSColorOutput/Output/TimeStamp/TimeStampMargin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace VSColorOutput.Output.TimeStamp
+{
+    public class TimeStampMargin : Canvas, IWpfTextViewMargin
+    {
+        private IWpfTextView _textView;
+        private TimeStampMarginProvider _timeStampMarginProvider;
+
+        public TimeStampMargin(IWpfTextView textView, TimeStampMarginProvider timeStampMarginProvider)
+        {
+            _textView = textView;
+            _timeStampMarginProvider = timeStampMarginProvider;
+        }
+
+        public ITextViewMargin GetTextViewMargin(string marginName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double MarginSize => ActualHeight;
+        public bool Enabled { get; } = true;
+        public FrameworkElement VisualElement => this;
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ VSColorOutput/Output/TimeStamp/TimeStampMarginProvider.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.Composition;
+using Microsoft.VisualStudio.Text.Classification;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.Utilities;
+
+namespace VSColorOutput.Output.TimeStamp
+{
+    [Export(typeof(IWpfTextViewMarginProvider))]
+    [Name("TimeStampMargin")]
+    [MarginContainer("LeftSelection")]
+    [Order(Before = "Spacer")]
+    [ContentType("DebugOutput")]
+    [TextViewRole("INTERACTIVE")]   
+    public class TimeStampMarginProvider : IWpfTextViewMarginProvider
+    {
+        public TimeStampMarginProvider()
+        {
+        }
+
+        [Import]
+        internal IClassificationFormatMapService ClassificationFormatMappingService { get; private set; }
+
+        [Import]
+        internal IClassificationTypeRegistryService ClassificationTypeRegistryService { get; private set; }
+
+        public IWpfTextViewMargin CreateMargin(IWpfTextViewHost textViewHost, IWpfTextViewMargin containerMargin)
+        {
+            return new TimeStampMargin(textViewHost.TextView, this);
+        }
+    }
+}
