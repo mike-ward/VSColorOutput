a2b562f123d6f29b3a55dbaa8cdf9aec9e9de7fb
mike-ward <mike@mike-ward.net>
2016-10-29 08:07:26 -0400

    migrate to vs2017, bump version

--- VSColorOutput/Properties/AssemblyInfo.cs
+++ VSColorOutput/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("VSColorOutput is a trademark of Mike Ward")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.4")]
-[assembly: AssemblyFileVersion("2.4")]
+[assembly: AssemblyVersion("2.5")]
+[assembly: AssemblyFileVersion("2.5")]
 [assembly: Guid("9D5A1B6F-D173-424E-B126-7A459F13DB7A")]
 
