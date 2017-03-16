dfa10579769aac9eb42f3a0c52a3658cbc65edd4
Carlos Henrique Guardão Gandarez <gandarez@gmail.com>
2017-03-16 14:03:58 -0300

    v2.6

--- Tests/Properties/AssemblyInfo.cs
+++ Tests/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("2.5")]
-[assembly: AssemblyFileVersion("2.5")]
+[assembly: AssemblyVersion("2.6")]
+[assembly: AssemblyFileVersion("2.6")]
