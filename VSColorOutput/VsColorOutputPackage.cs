e71b02253301913348b6f72bff11a1b4846faec1
Joe <dygood@outlook.com>
2022-11-11 20:19:53 +0800

    change version

--- VSColorOutput/VsColorOutputPackage.cs
+++ VSColorOutput/VsColorOutputPackage.cs
@@ -11,7 +11,7 @@ namespace VSColorOutput
     [Guid(PackageGuidString)]
     [ProvideOptionPage(typeof(VsColorOutputOptionsDialog), VsColorOutputOptionsDialog.Category, VsColorOutputOptionsDialog.SubCategory, 1000, 1001, true)]
     [ProvideProfile(typeof(VsColorOutputOptionsDialog), VsColorOutputOptionsDialog.Category, VsColorOutputOptionsDialog.SubCategory, 1000, 1001, true)]
-    [InstalledProductRegistration("VSColorOutput64", "Color output for build and debug windows - https://mike-ward.net/vscoloroutput", "2022.2")]
+    [InstalledProductRegistration("VSColorOutput64", "Color output for build and debug windows - https://mike-ward.net/vscoloroutput", "2022.11")]
     public sealed class VSColorOutputPackage : AsyncPackage
     {
         public const string PackageGuidString = "65dd734b-180a-4c67-b245-56de889637e1";
